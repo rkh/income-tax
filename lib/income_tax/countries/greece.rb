3d87a6a8033c6bd44393700b8a0856a06ad4b4db
Konstantinos Rousis <rousisk@gmail.com>
2015-11-10 16:23:14 +0100

    use personal income tax instead of corporation taxes for Greece

--- lib/income_tax/countries/greece.rb
+++ lib/income_tax/countries/greece.rb
@@ -4,8 +4,9 @@ module IncomeTax
       register "Greece", "GR", "GRC"
       currency "EUR"
 
-      level 50000, "26%"
-      remainder "33%"
+      level 25_000, "22%"
+      level 42_000, "32%"
+      remainder     "42%"
     end
   end
 end
