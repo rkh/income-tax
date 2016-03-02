f73cc108e1f96709a4b4bba91efda9403f15fd5f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2016-03-02 21:29:46 +0100

    fix rates for norway

--- lib/income_tax/countries/norway.rb
+++ lib/income_tax/countries/norway.rb
@@ -4,10 +4,12 @@ module IncomeTax
       register "Norway", "NO", "NOR"
       currency "NOK"
 
-      level 84420,  "0%"
-      level 456400, "28%"
-      level 741700, "37%"
-      remainder "40%"
+      fixed = Rate.new("25%")
+      level 159_800, fixed
+      level 224_900, fixed + Rate.new("0.44%")
+      level 565_400, fixed + Rate.new("1.7%")
+      level 909_500, fixed + Rate.new("10.7%")
+      remainder      fixed + Rate.new("13.7%")
     end
   end
 end
--- spec/income_tax/countries/norway_spec.rb
+++ spec/income_tax/countries/norway_spec.rb
@@ -16,37 +16,28 @@ describe IncomeTax::Countries::Norway do
     describe "from gross income of 1000" do
       let(:tax_year)     { 2015                                      }
       let(:income)       { 1000                                      }
-      its(:rate)         { should be == Rational(0, 1)               }
+      its(:rate)         { should be == Rational(1, 4)               }
       its(:gross_income) { should be == 1000                         }
-      its(:net_income)   { should be == 1000                         }
-      its(:taxes)        { should be == 0                            }
+      its(:net_income)   { should be == 750                          }
+      its(:taxes)        { should be == 250                          }
     end
 
     describe "from gross income of 10000" do
       let(:tax_year)     { 2015                                      }
       let(:income)       { 10000                                     }
-      its(:rate)         { should be == Rational(0, 1)               }
+      its(:rate)         { should be == Rational(1, 4)               }
       its(:gross_income) { should be == 10000                        }
-      its(:net_income)   { should be == 10000                        }
-      its(:taxes)        { should be == 0                            }
-    end
-
-    describe "from gross income of 100000" do
-      let(:tax_year)     { 2015                                      }
-      let(:income)       { 100000                                    }
-      its(:rate)         { should be == Rational(13, 298)            }
-      its(:gross_income) { should be == 100000                       }
-      its(:net_income)   { should be == "95637.6".to_d               }
-      its(:taxes)        { should be == "4362.4".to_d                }
+      its(:net_income)   { should be == 7500                         }
+      its(:taxes)        { should be == 2500                         }
     end
 
     describe "from gross income of 100000000" do
       let(:tax_year)     { 2015                                      }
       let(:income)       { 100000000                                 }
-      its(:rate)         { should be == Rational(91, 228)            }
+      its(:rate)         { should be == Rational(95, 246)            }
       its(:gross_income) { should be == 100000000                    }
-      its(:net_income)   { should be == "60086964.6".to_d            }
-      its(:taxes)        { should be == "39913035.4".to_d            }
+      its(:net_income)   { should be == "61381707.86".to_d           }
+      its(:taxes)        { should be == "38618292.14".to_d           }
     end
 
     describe "from net income of 0" do
@@ -57,46 +48,6 @@ describe IncomeTax::Countries::Norway do
       its(:gross_income) { should be == 0                            }
       its(:net_income)   { should be == 0                            }
       its(:taxes)        { should be == 0                            }
-    end
-
-    describe "from net income of 1000" do
-      let(:type)         { :net                                      }
-      let(:tax_year)     { 2015                                      }
-      let(:income)       { 1000                                      }
-      its(:rate)         { should be == Rational(0, 1)               }
-      its(:gross_income) { should be == 1000                         }
-      its(:net_income)   { should be == 1000                         }
-      its(:taxes)        { should be == 0                            }
-    end
-
-    describe "from net income of 10000" do
-      let(:type)         { :net                                      }
-      let(:tax_year)     { 2015                                      }
-      let(:income)       { 10000                                     }
-      its(:rate)         { should be == Rational(0, 1)               }
-      its(:gross_income) { should be == 10000                        }
-      its(:net_income)   { should be == 10000                        }
-      its(:taxes)        { should be == 0                            }
-    end
-
-    describe "from net income of 100000" do
-      let(:type)         { :net                                      }
-      let(:tax_year)     { 2015                                      }
-      let(:income)       { 100000                                    }
-      its(:rate)         { should be == Rational(65, 1138)           }
-      its(:gross_income) { should be == "106058.888888888889".to_d   }
-      its(:net_income)   { should be == 100000                       }
-      its(:taxes)        { should be == "6058.8888888888889".to_d    }
-    end
-
-    describe "from net income of 100000000" do
-      let(:type)         { :net                                      }
-      let(:tax_year)     { 2015                                      }
-      let(:income)       { 100000000                                 }
-      its(:rate)         { should be == Rational(151, 378)           }
-      its(:gross_income) { should be == "166521725.666666667".to_d   }
-      its(:net_income)   { should be == 100000000                    }
-      its(:taxes)        { should be == "66521725.6666666667".to_d   }
     end
   end
 end
