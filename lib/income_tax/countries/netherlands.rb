2aa25f052da454f9d8d2d6f7fe370333128e8df6
Bart de Water <bart@somnilocode.nl>
2016-08-07 21:33:33 +0200

    Update the Netherlands

--- lib/income_tax/countries/netherlands.rb
+++ lib/income_tax/countries/netherlands.rb
@@ -4,10 +4,48 @@ module IncomeTax
       register "Netherlands", "NL", "NLD"
       currency "EUR"
 
-      level 19645, "5.85%"
-      level 33363, "10.85%"
-      level 55991, "42%"
-      remainder "52%"
+      wants_options :birthday
+
+      levels year: 2016 do
+        level 19_922, "36.55%"
+        level 33_715, "40.4%"
+        level 66_421, "40.4%"
+        remainder "52%"
+      end
+
+      levels :fixed_aow_age, year: 2016 do
+        level 19_922, "18.65%"
+        level 34_027, "22.5%"
+        level 66_421, "40.4%"
+        remainder "52%"
+      end
+
+      levels :progressive_aow_age, year: 2016 do
+        level 19_922, "18.65%"
+        level 33_715, "22.5%"
+        level 66_421, "40.4%"
+        remainder "52%"
+      end
+
+      def level_category
+        if fixed_aow_age_reached?
+          :fixed_aow_age
+        elsif progressive_aow_age_reached?
+          :progressive_aow_age
+        else
+          :default
+        end
+      end
+
+      private
+
+      def fixed_aow_age_reached?
+        birthday < Date.new(1946, 1, 1) if birthday
+      end
+
+      def progressive_aow_age_reached?
+        birthday < Date.new(1951, 6, 30) if birthday
+      end
     end
   end
 end
